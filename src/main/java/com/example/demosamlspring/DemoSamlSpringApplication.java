14fc4892765221014b0f71423924f83b5a2935fb
Saurya Raj Pandey <sauryap77@gmail.com>
2022-02-22 20:24:13 +0545

    Spring booy using okta(saml)

---
+++ src/main/java/com/example/demosamlspring/DemoSamlSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.demosamlspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSamlSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSamlSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demosamlspring/DemoSamlSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demosamlspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSamlSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
